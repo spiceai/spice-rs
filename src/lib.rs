d58c836795cc1694188604c996373b64ae8622ce
Mitch Devenport <mitch@spice.ai>
2024-01-26 15:38:45 -0500

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ mod tls;
 
 pub use client::SpiceClient as Client;
 pub use prices::{HistoricalPriceData, LatestPriceDetail, LatestPricesResponse};
+pub use tls::new_tls_flight_channel;
 
 // Further public exports and integrations
 pub use futures::StreamExt;
