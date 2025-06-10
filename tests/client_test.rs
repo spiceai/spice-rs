a8e7dfcf395e0e1279979bd9c89d1f49b59fc5de
Sevenannn <qianqliu@uw.edu>
2025-06-10 11:34:46 -0700

    test_query_streaming

--- tests/client_test.rs
+++ tests/client_test.rs
@@ -49,4 +49,37 @@ mod tests {
             }
         };
     }
+
+    #[tokio::test]
+    async fn test_query_streaming() {
+        let mut spice_client = new_cloud_client().await;
+        match spice_client
+            .query(
+                "select VendorID, trip_distance, tpep_pickup_datetime from taxi_trips limit 10000",
+            )
+            .await
+        {
+            Ok(mut flight_data_stream) => {
+                // Read back RecordBatches
+                let mut num_batches = 0;
+                let mut total_rows = 0;
+                while let Some(batch) = flight_data_stream.next().await {
+                    match batch {
+                        Ok(batch) => {
+                            num_batches += 1;
+                            total_rows += batch.num_rows();
+                        }
+                        Err(e) => {
+                            panic!("Error: {e}")
+                        }
+                    };
+                }
+                assert_eq!(total_rows, 10000);
+                assert_ne!(num_batches, 1);
+            }
+            Err(e) => {
+                panic!("Error: {e}");
+            }
+        };
+    }
 }
