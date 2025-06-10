6282ec13cd27c8781e570a5cbd6e99670e3038ef
Sevenannn <qianqliu@uw.edu>
2025-06-09 20:34:45 -0700

    Add integration test for spice-rs `query` and `query_with_params`

--- src/tls.rs
+++ src/tls.rs
@@ -1,4 +1,7 @@
+use base64::{engine::general_purpose, Engine as _};
 use std::str::FromStr;
+
+use std::io::Write;
 use tonic::transport::channel::{ClientTlsConfig, Endpoint};
 use tonic::transport::Channel;
 
@@ -6,18 +9,17 @@ use crate::config::GenericError;
 
 pub fn system_tls_certificate() -> Result<tonic::transport::Certificate, GenericError> {
     // Load root certificates found in the platform’s native certificate store.
-    let certs = rustls_native_certs::load_native_certs()?;
+    // Use the same pem format as spiceai cloud connector: https://github.com/spiceai/spiceai/blob/571007c4be89a2a9892e3bd0eb43f8bd28464a69/crates/flight_client/src/tls.rs#L47
+    let cert_result = rustls_native_certs::load_native_certs();
 
-    let concatenated_pems = certs
-        .iter()
-        .filter_map(|cert| {
-            let mut buf = &cert.0[..];
-            rustls_pemfile::certs(&mut buf).ok()?.pop()
-        })
-        .map(String::from_utf8)
-        .collect::<Result<String, _>>()?;
+    let mut pem = Vec::new();
+    for cert in cert_result.certs {
+        pem.write_all(b"-----BEGIN CERTIFICATE-----\n")?;
+        pem.write_all(general_purpose::STANDARD.encode(cert.as_ref()).as_bytes())?;
+        pem.write_all(b"\n-----END CERTIFICATE-----\n")?;
+    }
 
-    Ok(tonic::transport::Certificate::from_pem(concatenated_pems))
+    Ok(tonic::transport::Certificate::from_pem(pem))
 }
 
 pub async fn new_tls_flight_channel(https_url: &str) -> Result<Channel, GenericError> {
